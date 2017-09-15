f6506aff509eaeb6c9a2281d7f450b6929417de9
Svetlin Kozhuharov <svetlin.kozhuharov@gmail.com>
2017-09-15 18:09:46 +0300

    Revert "Migrating web app"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,8 @@
-import { Routes, RouterModule } from '@angular/router';
-import { NoContent } from './no-content';
-import { IssuesComponent } from './issues';
-import { DashboardComponent } from './dashboard';
-import { ProfileComponent } from './profile';
-import { SigninComponent } from './signin';
+import { Routes } from '@angular/router';
 /**
  * Define app module routes here, e.g., to lazily load a module
  * (do not place feature module routes here, use an own -routing.module.ts in the feature instead)
  */
 export const AppRoutes: Routes = [
-    { path: '',   redirectTo: '/dashboard', pathMatch: 'full' },
-    { path: 'issues',  component: IssuesComponent },
-    { path: 'dashboard',  component: DashboardComponent },
-    { path: 'profile',  component: ProfileComponent },
-    { path: 'signin',  component: SigninComponent }
+    { path: '', pathMatch: 'full', redirectTo: '/home' }
 ];
